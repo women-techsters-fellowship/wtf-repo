edf93a98b47854f1af7f3a7bf94bdc5c04d69d99
katusabe scovia <scoviak588@gmail.com>
2025-10-15 15:34:01 +0300

    done with a class assignment

---
+++ scovia.py
@@ -0,0 +1,14 @@
+def introduce_myself():
+    first_name = "Scovia"
+    last_name = "Katusabe"
+    introduction = (
+        "I am a software engineer, DevOps student who loves building web applications "
+        "and exploring new technologies. I enjoy learning and solving real-world problems through code."
+    )
+
+    print("First Name:", first_name)
+    print("Last Name:", last_name)
+    print("About Me:", introduction)
+
+
+introduce_myself()
