ec641f6d8eacd6aca2541ebdd9f0d7e88af57d3b
Liberty <faklib3@gmail.com>
2025-10-15 13:11:41 +0100

    liberty's classwork

---
+++ libby.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script: libby.sh
+# Description: A short description about Liberty
+# Author: Liberty Fakolade
+# Date: October 15, 2025
+
+libby() {
+    firstname="Liberty"
+    lastname="Fakolade"
+    fullname="$firstname $lastname"
+    passion="learning hard things"
+    belief="nothing is impossible"
+    goal="DevOps"
+    
+    echo "WTF Classwork"
+    echo
+    echo "About Me"
+    echo
+    sleep 3
+    echo "My name is $fullname"
+    echo
+    sleep 3
+    echo "Not-So-Fun Facts About Me"
+    echo
+    sleep 3
+    echo "1. I love $passion"
+    echo
+    sleep 2
+    echo "2. I believe $belief"
+    echo
+    sleep 2
+    echo "3. You can call me Libby. Add Engr if you like (wink)"
+    echo
+    sleep 2
+    echo "4. Can't wait to be really good at this $goal thing"
+    sleep 2
+}
+libby
+
