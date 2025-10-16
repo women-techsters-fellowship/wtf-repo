56d0392d8a48393853d59a779c15017a5e25233a
CyGold <goldeous4@gmail.com>
2025-10-16 14:39:52 +0100

    Add aisha.sh script

---
+++ aisha.sh
@@ -0,0 +1,16 @@
+#!/bin/bash     
+
+aisha (){
+    echo "Hi there, my name is Aisha Yusuf"
+    sleep 3
+    echo "Im a tech enthusiast and python programmer"
+    sleep 3
+    echo "fun facts about me!!"
+    sleep 5
+    echo "- I love coding and automation."
+    echo "- I enjoy reading."
+    echo "- I can solve a Rubik's cube in 2 minute"
+    echo "- my favourite color is green"
+
+}
+aisha
