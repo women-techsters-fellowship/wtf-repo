4eefc1e7557f497e50a645d2738909022e6e5961
tabby123-dev <tabithawanjikundungu@gmail.com>
2025-10-16 14:34:20 +0300

    sript option of the task

---
+++ tabby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define variables
+first_name="Tabitha"
+last_name="Wanjiku"
+meet_me="I enjoy solving problems with code and I am finding great joy in learning DevOps as well as mastering cloud technologies."
+
+# Display information
+echo "My name is $first_name $last_name."
+echo "$meet_me"
