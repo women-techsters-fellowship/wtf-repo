390707544c5287d9621b06a632ed6d96f60cb8ad
Queenestydev <akachukwuqueenesther93@gmail.com>
2025-10-16 02:40:32 +0100

    a script about myself and what i do

---
+++ Queenesty2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+##### Author ######
+# Forget  about the date#
+
+# Date= 16th, oct.2025 #
+
+
+function  NAME(){
+
+FIRST_NAME="Queenesther"
+LAST_NAME="Caleb"
+
+echo "hello, my name is $FIRST_NAME $LAST_NAME, do not play with me though, but i'm a chill girl."
+
+}
+
+NAME
+
+function Bio(){
+
+FULL_NAME="Queenesther Caleb"
+JOB="DevOps Engineer"
+
+echo "$FULL_NAME  is an igbo girl and intrestingly, a $JOB too".
+
+}
+
+Bio
+
+sleep 1
+echo "Mr Peter is a $JOB."
+sleep 1
+echo "But me, i am Mr Peter's $JOB trainee."
+
+
+function combined(){
+
+FIRST_NAME="Queenesther"
+LAST_NAME="Caleb"
+
+echo "my name is $FIRST_NAME $LAST_NAME."
+sleep 2
+echo "$FULL_NAME is an igbo girl and a $JOB too".
+
+}
+
+
+combined
+
