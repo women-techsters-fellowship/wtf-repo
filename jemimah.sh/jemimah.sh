eeb0964a6f227e96e48a6107a6ebdd6e1a21d3ec
jemimahrimdan <jemimahrimdan0@gmail.com>
2025-10-16 10:57:36 +0100

    Added personal info script

---
+++ jemimah.sh/jemimah.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Function to display personal information
+show_info() {
+  echo "Name: Jemimah Rimdan"
+  echo "Profession: Technical Product Manager and DevOps Engineer"
+  echo "Email: jemimahrimdan0@gmail.com"
+  echo "Nationality: Nigerian"
+  echo "State of Origin: [Plateau State]"
+  echo "Hobbies: hanging out, watching movies, Learning"
+  echo "Dislikes: Dishonesty, Laziness and bullying"
+}
+
+# Call the function
+show_info
