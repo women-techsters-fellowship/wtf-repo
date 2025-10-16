c7f92d87222445776695e906bc810cf32e811e82
Susan Amechi <amechisusanogechi@gmail.com>
2025-10-16 17:14:38 +0100

    Added my susan.sh file

--- susan.sh
+++ susan.sh
@@ -1,4 +1,4 @@
 function name(){
-    echo "My name is Susan Amechi and I'm a 300L Software Engineering Student studying at the Federal University Of Technology, Owerri"
+echo " My name is Susan Amechi and I'm a 300L Software Enginnering Student at the Federal University Of Technology, Owerri "
 }
 name
