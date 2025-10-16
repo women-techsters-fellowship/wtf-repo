8df9e6ca8c7805398be265b13e40e3b3db8120b5
Yvette Christine <yvettechristine245@gmail.com>
2025-10-16 13:49:53 +0300

    Added ivy-script.sh file

---
+++ ivy-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Function that displays info about me
+yvette_christine_info() {
+    echo "My name is Yvette Christine."
+    echo "I am a software developer from Nairobi, Kenya."
+    echo "I enjoy building projects using JavaScript and I cant wait to be a DevOps Engineer."
+}
+
+# Call the function
+yvette_christine_info
