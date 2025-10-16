2b7fb6532ddcb89f2e1fad57033ed20474eb4f90
WamaithaKinyua <rutwakin@gmail.com>
2025-10-16 15:20:12 +0300

    Add Ruth branch

---
+++ Ruth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Below is a function that mentions my name and says something about me.
+
+firstname="Ruth"
+lastname="Kinyua"
+funfact="I love swimming"
+
+# Pause for 5 seconds
+sleep 5
+
+{
+    echo "### Firstname: $firstname"
+    echo ""
+    echo "Lastname: $lastname"
+    echo ""
+    echo "Fun Fact: $funfact"
+    echo ""
+}
