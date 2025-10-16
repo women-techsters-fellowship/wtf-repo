12b969292b1c27cd2e3f9db79f7b09e40ce75c20
Emmanuella Uzochukwu <uzooge4@gmail.com>
2025-10-16 19:59:49 +0100

    Add about script with Emmanuella_Uzochukwu_info

---
+++ about_emmanuella.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#Function name contains name as requested
+Emmanuella_Uzochukwu_info() {
+  echo "First name: Emmanuella"
+  echo "Last name: Uzochukwu"
+  echo "About: Product Manager learning DevOps"
+}
+
+#Call the function when the script runs
+Emmanuella_Uzochukwu_info
