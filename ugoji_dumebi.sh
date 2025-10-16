be429c9fd5a9c37da61904be409f0c0cd0250419
vboxuser <ugojidumebi2692@gmail.com>
2025-10-16 11:15:25 +0100

    added ugoji_dumebi.sh with bio function

---
+++ ugoji_dumebi.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+describe_me(){
+echo "===================================="
+echo "      ABOUT ME"
+echo "Name: Chukwudumebi Ugoji"
+echo "Profession: Data Analyst"
+echo "Interest: Data,Technology and Automation"
+echo "FunFact:I enjoy making informed decisions from insight derived from analy>
+echo "Professional background:I have a background in Accounting from University>
+echo "====================================" 
+
+}
+
+describe_me
