f35a3e49633d73af70a3fe9bd6d94d56971fdd35
linda joseph <joslinda11@gmail.com>
2025-10-17 00:37:41 +0100

    added my script

---
+++ linda_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script Name: linda_script.sh
+# Author: linda joseph
+
+
+# function to display my info
+linda_info() {
+   echo "first name: linda"
+   echo "last  name: joseph"
+   echo "About me: Efficient DevOps Engineer helping teams automate,deploy and scale with confidence."
+}
+
+#call the function
+linda_info
