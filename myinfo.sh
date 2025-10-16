507cce73e2c2714f2422f5ca1f0fdb63334653aa
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-10-16 10:28:40 +0000

    about mayself

---
+++ myinfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+antwiwaa() {
+	echo "I'm Nana Afua Antwiwaa Conduah. First name: Nana Afua and last: Conduah"
+	echo "As a dedicated member of women techsters fellowship 2025,devops track,"
+	echo "I plan to use the skills so far for community development"
+}
+antwiwaa
