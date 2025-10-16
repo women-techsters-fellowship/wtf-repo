9c0adeb0ba8f1d2d3539663914944a61217f0c41
basiratkareem <basiratkareem18@gmail.com>
2025-10-16 13:13:10 +0100

    classwork submission

---
+++ basirat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+about_me() {
+
+first_name="Basirat"
+last_name="Kareem"
+role="QA Engineer"
+passion="Learning and growing personally and professionally"
+goal="To becaome a skilled and experienced DevOps Engineer"
+
+
+echo "Name: $first_name $last_name"
+echo "Role: $role"
+echo "Passion: $passion"
+echo "Goal: $goal"
+
+}
+
+about_me
