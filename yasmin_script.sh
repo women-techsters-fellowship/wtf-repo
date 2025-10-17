97eb83acc67dda6d03ab7322a82c000f389da9e4
yasmin <yasminsbii09@gmail.com>
2025-10-17 09:45:33 +0100

    Added Bash script with about_me function

---
+++ yasmin_script.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+# ==========================================
+# In class activity: Introduction about yourself
+# Description:
+#   Displays my first name, last name, and some information.
+# ==========================================
+
+about_me() {
+    first_name="Yasmin"
+    last_name="Sebei"
+    course="Computer Engineering - DevOps"
+    interest="Artificial Intelligence, Cloud Computing, and IoT"
+    hobby="Building smart systems and learning new technologies"
+
+    echo "=============================="
+    echo "         ABOUT ME"
+    echo "=============================="
+    echo "Name: $first_name $last_name"
+    echo "Course: $course"
+    echo "Interest: $interest"
+    echo "Hobby: $hobby"
+    echo "=============================="
+}
+about_me
