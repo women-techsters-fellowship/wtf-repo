d7be134983bfa830bbe40e3c6ef1cb711a4b7051
Yaa-K <yaa.k.yeboah@outlook.com>
2025-10-19 17:17:25 +0000

    Created a .sh file with my name and other details

---
+++ yaa-yeboah.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###Name:Yaa Kesewaa Yeboah #######
+###Date:19th October 2025#####
+###email:yaa.k.yeboah@outlook.com###
+
+
+echo "Hi my name is Yaa Kesewaa Yeboah"
+echo "I am from Ghana and I am super excited to be learning DevOps"
