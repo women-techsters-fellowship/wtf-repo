cd089a67e223b49ccd9c8346388ad1bfe3089582
Rosemary Edem <chummyrosie@gmail.com>
2025-10-19 13:49:02 +0100

    Added rosemary_edem_info.sh script with personal function

---
+++ rosemary_edem_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Rosemary Edem
+# Description: A script that displays my first name, last name, a few things about me and my career goals
+
+function rosemary_edem_info() {
+    echo "First Name: Rosemary"
+    echo "Last Name: Edem"
+    echo "I am a DevOps and cloud support trainee passionate about technology, automation and continuous learning."
+    echo "Fun fact: I enjoy helping others learn tech and improving systems efficiency!"
+    echo "Goal: By the end of my learning journey, I aim to become a highly skilled DevOps Support Engineer, confident in cloud operations , CI/CD automation and real-world troubleshooting, ready to contribute to impactful tech projects globally."
+}
+
+# Call the function
+rosemary_edem_info
