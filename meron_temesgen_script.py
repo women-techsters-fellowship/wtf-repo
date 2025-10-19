31d74f940e8bf4d87fc59c8a2889249e200c838a
Meron Temesgen Welu <merontemesgen142@gmail.com>
2025-10-19 23:31:31 +0300

    Added Meron Temesgen script

---
+++ meron_temesgen_script.py
@@ -0,0 +1,9 @@
+
+def meron_temesgen_info():
+    print("Name: Meron Temesgen Welu")
+    print("Program: Women Techsters Fellowship")
+    print("Track: DevOps")
+    print("About Me: I am passionate about technology, problem-solving, and empowering women through tech innovation.")
+
+# Call the function
+meron_temesgen_info()
