6477c3e11ca74d0fca97b6a7734278666e5a7f0a
Christiana Folorunsho <folorunshotolani07@gmail.com>
2025-10-19 21:01:13 +0000

    Added Christiana Folorunsho script

---
+++ about_christiana.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "My name is Christiana Folorunsho."
+echo "I am learning Devops through the Women Techsters Fellowship."
+echo "I love solving problems and learning new tech tools."
+echo "I enjoy technology, teamwork,and continuous learning."
