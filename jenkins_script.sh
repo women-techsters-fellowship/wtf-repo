69f55fb209afcc56eea0277fce5113c1ba42cdf4
Ukpabi Peter Uchenna <162847177+bigcephas1@users.noreply.github.com>
2025-10-29 11:41:56 +0100

    Create jenkins_script.sh

---
+++ jenkins_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+##### DATE: October 29 2025 ########
+
+#### Author: WTF ########
+
+#### Description: For testing jenkins Job with git #####
+
+echo "Jenkins  went to git to pull and run this script"
+
+sleep 10
+echo "BOOM, that's it"
