86409afdb533dd851ff395f361b3ea074f9291ca
Miranda.A <achiengmiranda14@gmail.com>
2025-11-06 13:44:24 +0300

    Add intro script

---
+++ m_achieng_intro.py
@@ -0,0 +1,13 @@
+# miranda_achieng_intro.py
+
+def display_miranda_achieng_info():
+    print("First Name: Miranda")
+    print("Last Name: Omondi")
+    print("\nA few things about me:")
+    print("- I am a university student in Kenya.")
+    print("- I'm excited to be here.")
+    print("- I enjoy music and playing the recorder and violin in my free time.")
+    print("- My goal is to be an efficient developer operator.")
+
+if __name__ == "__main__":
+    display_miranda_achieng_info()
