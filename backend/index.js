0e65090f775b6af09d4d2019b6f926265faa56ef
saraharike-tech <sarahobafaiye75@gmail.com>
2025-11-19 10:57:55 +0100

    Frontend + Backend setup

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running...");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
