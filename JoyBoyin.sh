fc0999494135ce9f03e6e9d0d16ce3ade2b5839a
joy-Boyinbode1 <boyinbodejoy@gmail.com>
2025-11-29 10:46:16 +0000

    Introduction script by Boyinbode Joy

---
+++ JoyBoyin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#function to display personal information
+
+display_info () {
+echo "==========================================="
+echo "      MY PERSONAL INFO                     "
+echo "==========================================="
+echo " Full Name                :Boyinbode Joy   "
+echo " Country                  :Nigeria         "
+echo " Lastname                 :Boyinbode       "
+echo " Current goal             :Purposeful life "
+echo " Hobbies                  :Sleeping,money  "
+echo "==========================================="
+echo " Thank you for checking my script          "
+}
+
+
+#Call the function
+display_info
